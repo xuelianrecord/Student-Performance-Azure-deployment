b6e3eec0488539ff33e7f8293aa056b7630dfa86
xuelianrecord <xuelianrecord@gmail.com>
2025-06-14 12:15:45 -0500

    add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="xuelianrecord",
+    author_email="xuelianrecord@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
